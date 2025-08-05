35f5dfbfafde48931305fda0a12739069b0d4a48
Bhargavvallapuram <147648796+Bhargavvallapuram@users.noreply.github.com>
2025-08-05 13:56:08 +0530

    Delete routes directory

--- routes/urlRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const urlController = require("../controllers/urlController");
-
-router.post("/", urlController.createShortUrl);
-router.get("/:shortcode", urlController.redirectShortUrl);
-router.get("/:shortcode/stats", urlController.getShortUrlStats);
-
-module.exports = router;
